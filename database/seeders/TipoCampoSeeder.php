f64b110aa7cc2a667a6e5838a1162d1254fa77f8
Pedro Costa <pedroh.costa083@gmail.com>
2024-06-05 13:29:17 -0300

    adicionando os tipos fixos de tipos de campos

--- database/seeders/TipoCampoSeeder.php
+++ database/seeders/TipoCampoSeeder.php
@@ -14,14 +14,38 @@ class TipoCampoSeeder extends Seeder
     {
         DB::table('tipos_campos')->insert([
             [
-                'nome' => 'Texto',
+                'nome' => 'text',
                 'descrição' => 'Campo de texto simples',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nome' => 'Seleção',
+                'nome' => 'select',
                 'descrição' => 'Campo com opções selecionáveis',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nome' => 'radio',
+                'descrição' => 'Botões de rádio onde o usuário pode selecionar apenas uma opção de um conjunto.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nome' => 'checkbox',
+                'descrição' => 'Caixa de seleção onde o usuário pode selecionar uma ou mais opções de um conjunto.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nome' => 'textarea',
+                'descrição' => 'Área de texto multilinha.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nome' => 'email',
+                'descrição' => 'Campo de entrada para endereço de e-mail.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
