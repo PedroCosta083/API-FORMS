0ae51e3f3f4783ac27fe96153d0aae6dc6418a94
Pedro Costa <pedroh.costa083@gmail.com>
2024-06-05 10:21:07 -0300

    criação das migrations

---
+++ database/migrations/2024_06_05_131008_create_campos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('descrição')->nullable();
+            $table->string('rotulo')->nullable();
+            $table->foreignId('formulario_id')->constrained('formularios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campos');
+    }
+};
