f7159917d64ed1e8144771c80deca16bac782052
Pedro Costa <pedroh.costa083@gmail.com>
2024-06-06 10:06:52 -0300

    atualização seeders TipoCampo

--- database/seeders/TipoCampoSeeder.php
+++ database/seeders/TipoCampoSeeder.php
@@ -15,37 +15,37 @@ class TipoCampoSeeder extends Seeder
         DB::table('tipos_campos')->insert([
             [
                 'nome' => 'text',
-                'descrição' => 'Campo de texto simples',
+                'descricao' => 'Campo de texto simples',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'nome' => 'select',
-                'descrição' => 'Campo com opções selecionáveis',
+                'descricao' => 'Campo com opções selecionáveis',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'nome' => 'radio',
-                'descrição' => 'Botões de rádio onde o usuário pode selecionar apenas uma opção de um conjunto.',
+                'descricao' => 'Botões de rádio onde o usuário pode selecionar apenas uma opção de um conjunto.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'nome' => 'checkbox',
-                'descrição' => 'Caixa de seleção onde o usuário pode selecionar uma ou mais opções de um conjunto.',
+                'descricao' => 'Caixa de seleção onde o usuário pode selecionar uma ou mais opções de um conjunto.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'nome' => 'textarea',
-                'descrição' => 'Área de texto multilinha.',
+                'descricao' => 'Área de texto multilinha.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'nome' => 'email',
-                'descrição' => 'Campo de entrada para endereço de e-mail.',
+                'descricao' => 'Campo de entrada para endereço de e-mail.',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
